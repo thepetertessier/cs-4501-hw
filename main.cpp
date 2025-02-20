5def0bfaae8a2afe552db9811c55b11e493e4275
thepetertessier <fpv5gr@virginia.edu>
2025-02-19 23:05:47 -0500

    custom Transformatino struct

--- main.cpp
+++ main.cpp
@@ -1,178 +1,154 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <sstream>
 #include <iomanip>
-
-#define MAX_N 100000
-#define MAX_Q 50000
+#include <stdexcept>
+#include <algorithm>
 
 using namespace std;
-using Matrix = array<array<double, 3>, 3>;
 
-inline Matrix get_identity() {
-    return {{
-        {1,0,0},
-        {0,1,0},
-        {0,0,1}
-    }};
+// Represent an affine transformation by six doubles.
+// This corresponds to the matrix:
+//    [ a   b   c ]
+//    [ d   e   f ]
+//    [ 0   0   1 ]
+struct Transformation {
+    double a, b, c, d, e, f;
+};
+
+inline Transformation get_identity() {
+    return {1, 0, 0, 0, 1, 0};
 }
 
-inline Matrix multiply_matrices(Matrix &A, Matrix &B) {
-    Matrix result = {};
-    for (int i = 0; i < 3; i++)
-        for (int k = 0; k < 3; k++)
-            for (int j = 0; j < 3; j++)
-                result[i][j] += A[i][k] * B[k][j];
-    return result;
+// Multiply two transformations T1 and T2.
+// Note: This gives the composition T1 * T2, meaning that if you
+// first apply T2 then T1, you get the result.
+inline Transformation multiply(const Transformation &T1, const Transformation &T2) {
+    return {
+        T1.a * T2.a + T1.b * T2.d,               // a
+        T1.a * T2.b + T1.b * T2.e,               // b
+        T1.a * T2.c + T1.b * T2.f + T1.c,        // c
+        T1.d * T2.a + T1.e * T2.d,               // d
+        T1.d * T2.b + T1.e * T2.e,               // e
+        T1.d * T2.c + T1.e * T2.f + T1.f         // f
+    };
 }
 
-inline unsigned int get_left_child_index(unsigned int index) {
-    return 2*index + 1;
-}
-
-inline unsigned int get_right_child_index(unsigned int index) {
-    return 2*index + 2;
-}
-
+// Segment Tree that stores Transformation objects.
 struct SegmentTree {
-    unsigned int n;
-    vector<Matrix> t_tranformations;
-
-    SegmentTree(vector<Matrix> a, unsigned int _n) {
-        n = _n;
-        t_tranformations = vector<Matrix>(4*MAX_N);
-        build(a);
+    int n;
+    vector<Transformation> tree;
+    
+    SegmentTree(const vector<Transformation>& arr) {
+        n = arr.size();
+        tree.resize(4 * n);
+        build(arr, 0, 0, n - 1);
     }
-
-    void build(vector<Matrix> a, int v, int tl, int tr) {
-        if (tl == tr) {
-            t_tranformations[v] = a[tl];
+    
+    void build(const vector<Transformation>& arr, int v, int tl, int tr) {
+        if(tl == tr) {
+            tree[v] = arr[tl];
         } else {
             int tm = (tl + tr) / 2;
-            int il = get_left_child_index(v);
-            int ir = get_right_child_index(v);
-            build(a, il, tl, tm);
-            build(a, ir, tm+1, tr);
-            t_tranformations[v] = multiply_matrices(t_tranformations[ir], t_tranformations[il]);
+            int left = 2*v + 1, right = 2*v + 2;
+            build(arr, left, tl, tm);
+            build(arr, right, tm+1, tr);
+            // Note: the order is important!
+            tree[v] = multiply(tree[right], tree[left]);
         }
     }
-
-    inline void build(vector<Matrix> a) {
-        build(a, 0, 0, n-1);
-    }
-
-    Matrix get_composed(int v, int tl, int tr, int l, int r) {
-        if (l > r) {
+    
+    Transformation query(int v, int tl, int tr, int l, int r) {
+        if(l > r)
             return get_identity();
-        }
-        if (l == tl && r == tr) {
-            return t_tranformations[v];
-        }
+        if(l == tl && r == tr)
+            return tree[v];
         int tm = (tl + tr) / 2;
-        auto L = get_composed(get_left_child_index(v), tl, tm, l, min(r, tm));
-        auto R = get_composed(get_right_child_index(v), tm+1, tr, max(tm+1, l), r);
-        return multiply_matrices(R, L);
+        Transformation leftRes = query(2*v+1, tl, tm, l, min(r, tm));
+        Transformation rightRes = query(2*v+2, tm+1, tr, max(l, tm+1), r);
+        return multiply(rightRes, leftRes);
     }
-
-    inline Matrix get_composed(int l, int r) {
-        return get_composed(0, 0, n-1, l, r);
+    
+    inline Transformation query(int l, int r) {
+        return query(0, 0, n-1, l, r);
     }
-
-    void update(int v, int tl, int tr, int pos, Matrix new_transformation) {
-        if (tl == tr) {
-            t_tranformations[v] = new_transformation;
+    
+    void update(int v, int tl, int tr, int pos, const Transformation &newVal) {
+        if(tl == tr) {
+            tree[v] = newVal;
         } else {
             int tm = (tl + tr) / 2;
-            int il = get_left_child_index(v);
-            int ir = get_right_child_index(v);
-            if (pos <= tm) {
-                update(il, tl, tm, pos, new_transformation);
-            } else {
-                update(ir, tm+1, tr, pos, new_transformation);
-            }
-            t_tranformations[v] = multiply_matrices(t_tranformations[ir], t_tranformations[il]);
+            if(pos <= tm)
+                update(2*v+1, tl, tm, pos, newVal);
+            else
+                update(2*v+2, tm+1, tr, pos, newVal);
+            tree[v] = multiply(tree[2*v+2], tree[2*v+1]);
         }
     }
-
-    inline void update(int pos, Matrix matrix) {
-        update(0, 0, n-1, pos, matrix);
+    
+    inline void update(int pos, const Transformation &newVal) {
+        update(0, 0, n-1, pos, newVal);
     }
 };
 
-inline tuple<double, double, double> apply_matrix(Matrix A, int x, int y, int z) {
-    double new_x = A[0][0] * x + A[0][1] * y + A[0][2] * z;
-    double new_y = A[1][0] * x + A[1][1] * y + A[1][2] * z;
-    double new_z = A[2][0] * x + A[2][1] * y + A[2][2] * z;
-    return {new_x, new_y, new_z};
-}
-
-inline pair<double, double> apply_transformations(int x, int y, int l, int r, SegmentTree &st) {
-    auto composed_transformation = st.get_composed(l, r);
-    auto [new_x, new_y, new_z] = apply_matrix(composed_transformation, x, y, 1);
+// Apply a transformation to the point (x, y).
+inline pair<double, double> apply(const Transformation &T, int x, int y) {
+    double new_x = T.a * x + T.b * y + T.c;
+    double new_y = T.d * x + T.e * y + T.f;
     return {new_x, new_y};
 }
 
-Matrix get_tranformation_matrix_from_cin(std::string &op) {
-    if (op == "Translate") {
+// Read a transformation from input.
+Transformation read_transformation() {
+    string op;
+    cin >> op;
+    if(op == "Translate") {
         double dx, dy;
         cin >> dx >> dy;
-        return {{
-            {  1,   0,  dx},
-            {  0,   1,  dy},
-            {  0,   0,   1}
-        }};
-    } else if (op == "Scale") {
+        return {1, 0, dx, 0, 1, dy};
+    } else if(op == "Scale") {
         double sx, sy;
         cin >> sx >> sy;
-        return {{
-            { sx,   0,   0},
-            {  0,  sy,   0},
-            {  0,   0,   1}
-        }};
-    } else if (op == "Rotate") {
+        return {sx, 0, 0, 0, sy, 0};
+    } else if(op == "Rotate") {
         double d;
         cin >> d;
-        d *= (M_PI / 180); // convert to radians
-        return {{
-            { cos(d),-sin(d), 0},
-            { sin(d), cos(d), 0},
-            {      0,      0, 1}
-        }};
+        double rad = d * (M_PI / 180.0);
+        double cosd = cos(rad), sind = sin(rad);
+        return {cosd, -sind, 0, sind, cosd, 0};
     } else {
         throw invalid_argument("Unrecognized matrix operation.");
     }
 }
 
 int main() {
-    unsigned int n, q;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n, q;
     cin >> n >> q;
-
-    vector<Matrix> a_transformations(MAX_N);
-
-    for (unsigned int i = 0; i < n; i++) {
-        string op;
-        cin >> op;
-        a_transformations[i] = get_tranformation_matrix_from_cin(op);
+    vector<Transformation> arr(n);
+    for (int i = 0; i < n; i++) {
+        arr[i] = read_transformation();
     }
-
-    SegmentTree segment_tree(a_transformations, n);
-
-    for (unsigned int i = 0; i < q; i++) {
+    
+    SegmentTree seg(arr);
+    
+    cout << fixed << setprecision(5);
+    for (int i = 0; i < q; i++) {
         char type;
         cin >> type;
-        if (type == 'Q') {
+        if(type == 'Q') {
             int x, y, l, r;
             cin >> x >> y >> l >> r;
-            auto [new_x, new_y] = apply_transformations(x, y, l, r, segment_tree);
-            cout << fixed << setprecision(5);
-            cout << "(" << x << "," << y << "): " << new_x << " " << new_y << "\n";
-        } else if (type == 'U') {
+            Transformation comp = seg.query(l, r);
+            auto res = apply(comp, x, y);
+            cout << "(" << x << "," << y << "): " << res.first << " " << res.second << "\n";
+        } else if(type == 'U') {
             int pos;
-            string op;
-            cin >> pos >> op;
-            auto new_tranformation = get_tranformation_matrix_from_cin(op);
-            segment_tree.update(pos, new_tranformation);
+            cin >> pos;
+            Transformation newTrans = read_transformation();
+            seg.update(pos, newTrans);
         }
     }
     return 0;
